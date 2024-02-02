2d0229a19246ab03126c3348a65b49979119caaf
lucaslara4 <lucaslara.salgado@gmail.com>
2024-02-02 16:25:39 -0300

    2nd

--- UF calculator.py
+++ UF calculator.py
@@ -1,33 +1,18 @@
-# CHILEAN UF CALCULATOR
-
 import calendar
 from datetime import datetime
 
 def obtener_valor_uf():
-    UFanterior = 36500  
+    UFanterior = 36500  # MODIFICAR MANUALMENTE
     fecha_actual = datetime.now()
-    fecha_utima_UF = datetime(2024, 1, 9) 
-    cantidad_dias_mes_IPCanterior = obtener_cantidad_dias_mes_IPCanterior() 
-    variacion_IPC = 0.002  
+    fecha_utima_UF = datetime(2024, 1, 9) # MODIFICAR MANUALMENTE
+    fecha_ultimo_IPC = datetime(2024, 1, 8) # MODIFICAR MANUALMENTE
+    cantidad_dias_mes_IPCanterior = calendar.monthrange(fecha_ultimo_IPC.year, fecha_ultimo_IPC.month)[1]
+    variacion_IPC = 0.002  # MODIFICAR MANUALMENTE
 
     UF_actual = UFanterior * (1 + variacion_IPC) ** ((fecha_actual - fecha_utima_UF).days / cantidad_dias_mes_IPCanterior)
+    UF_actual = round(UF_actual, 2)
 
     print(f"El valor actual de la UF Chilena es {UF_actual:.2f}")
 
-def obtener_cantidad_dias_mes_IPCanterior():
-    # Obtén el mes y año actuales
-    mes_actual = datetime.now().month
-    año_actual = datetime.now().year
-
-    # Si estás calculando para el mes anterior, resta 1 al mes actual
-    mes_IPCanterior = mes_actual - 1 if mes_actual > 1 else 12
-    año_IPCanterior = año_actual if mes_actual > 1 else año_actual - 1
-
-    # Obtiene la cantidad de días en el mes del IPC anterior
-    cantidad_dias = calendar.monthrange(año_IPCanterior, mes_IPCanterior)[1]
-
-    return cantidad_dias
-
 if __name__ == "__main__":
     obtener_valor_uf()
-    cantidad_dias_mes_IPCanterior = obtener_cantidad_dias_mes_IPCanterior()
